ee4e2920d667de5d9bb7655dbbbbf0bccedcf04d
Mustafa <mustafaymustafa@icloud.com>
2022-10-09 18:04:14 +0100

    graph test skeletons

---
+++ tests/test_graph.py
@@ -0,0 +1,14 @@
+def test_init_graph():
+    pass
+
+def test_repr():
+    pass
+
+def test_add_edge():
+    pass
+
+def test_graph_int_verticies():
+    pass
+
+def test_graph_str_verticies():
+    pass
