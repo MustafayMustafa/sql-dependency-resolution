2ebe369557f7753e28247f9fc6a8ae39d5b25ef5
Mustafa <mustafaymustafa@icloud.com>
2022-10-16 22:36:54 +0100

    test create and drop order methods

---
+++ tests/test_resolver.py
@@ -0,0 +1,24 @@
+import pytest
+
+from src.resolver import DependencyResolver
+
+def test_valid_sorting():
+    views = "tests/data/"
+    resolver = DependencyResolver(objects_path=views)
+
+    assert resolver.create_order() == [
+        "vw_view_3",
+        "vw_view_2",
+        "vw_view_1",
+        "vw_view_4",
+    ]
+    assert resolver.drop_order() == [
+        "vw_view_4",
+        "vw_view_1",
+        "vw_view_2",
+        "vw_view_3",
+    ]
+@pytest.mark.skip
+# TODO: mock files to add extra view that causes circular dependency
+def test_circular_dependency():
+    assert False
