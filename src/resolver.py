d5c83a63a05e22385925f05682f7c9daa4912f28
Mustafa <mustafaymustafa@icloud.com>
2022-10-16 23:00:52 +0100

    doc strings and type hints

--- src/resolver.py
+++ src/resolver.py
@@ -9,7 +9,13 @@ from src.topological_sort import TopologicalSortMixin
 
 
 class DependencyResolver(TopologicalSortMixin):
-    def __init__(self, objects_path) -> None:
+    """Resolver class implements a create and drop method providing the conflict-free ordering for creation and destruction of SQL objects.
+
+    Args:
+        TopologicalSortMixin (class): sort mixin for topological sorting.
+    """
+
+    def __init__(self, objects_path: str) -> None:
         self.graph = Graph()
         self.files = [
             join(objects_path, f)
@@ -19,7 +25,8 @@ class DependencyResolver(TopologicalSortMixin):
         self.create_nodes()
         self.order = self.sort(graph=self.graph.graph, size=self.graph.size)
 
-    def create_nodes(self):
+    def create_nodes(self) -> None:
+        """Create SQL nodes and append to dependency graph."""
         for file in self.files:
             node = ViewNode(file)
             name = Path(file).stem
@@ -27,7 +34,17 @@ class DependencyResolver(TopologicalSortMixin):
                 self.graph.add_edge(name, dependency)
 
     def create_order(self) -> List:
+        """The order to create SQL objects.
+
+        Returns:
+            List: creation order.
+        """
         return list(reversed(self.order))
 
     def drop_order(self) -> List:
+        """The order to destroy the SQL objects.
+
+        Returns:
+            List: destruction order.
+        """
         return self.order
