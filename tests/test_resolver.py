5dbc31d124c28a5b0224e38acb21bedeea01a67b
Mustafa <mustafaymustafa@icloud.com>
2022-10-17 23:08:22 +0100

    test circular dependency on resolver class

--- tests/test_resolver.py
+++ tests/test_resolver.py
@@ -1,13 +1,17 @@
-import pytest
+import shutil
 
+import pytest
 from src.resolver import DependencyResolver
+from src.topological_sort import CircularDependencyException
+
 
 def test_resolver_in_degree():
     views = "tests/data/views/"
     resolver = DependencyResolver(objects_path=views)
-    degrees =  resolver.in_degrees(resolver.graph.graph, None)
+    degrees = resolver.in_degrees(resolver.graph.graph, None)
 
-    assert degrees == {'vw_view_1': 1, 'vw_view_2': 1, 'vw_view_3': 0, 'vw_view_4': 2}
+    assert degrees == {"vw_view_1": 1, "vw_view_2": 1, "vw_view_3": 0, "vw_view_4": 2}
+
 
 def test_valid_sorting():
     views = "tests/data/views/"
@@ -27,7 +31,33 @@ def test_valid_sorting():
     ]
 
 
-@pytest.mark.skip
-# TODO: mock files to add extra view that causes circular dependency
-def test_circular_dependency():
-    assert False
+def test_circular_dependency(tmp_path):
+    source_dir = "tests/data/views"
+    destination_dir = tmp_path / "sub"
+    shutil.copytree(source_dir, destination_dir)
+
+    vw_view_5 = """
+    create view vw_view_5 as
+    select * 
+    from some_table st
+    join vw_view_6 sv on
+        sv.id = st.id
+    """
+
+    vw_view_6 = """
+    create view vw_view_5 as
+    select * 
+    from some_table st
+    join vw_view_5 sv on
+        sv.id = st.id
+    """
+
+    with open(f"{destination_dir}/vw_view_5.txt", "a") as f:
+        f.write(vw_view_5)
+    with open(f"{destination_dir}/vw_view_6.txt", "a") as f:
+        f.write(vw_view_6)
+
+    with pytest.raises(CircularDependencyException) as e:
+        resolver = DependencyResolver(objects_path=destination_dir)
+        resolver.create_order()
+    assert str(e.value) == "Cycle detected"
